f4ea3fd54ccf46f5086a737c317293d5a36b7aa6
James Sherry <james.m.sherry@googlemail.com>
2023-04-13 17:23:20 +0100

    changed host and server instantiation

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const app = require('./server');
 
 const {
   PORT=3333,
-  LOCAL=true
+  HOST='127.0.0.1'
 } = process.env;
-
-
-const HOST = LOCAL === true ? '127.0.0.1' : '0.0.0.0';
 
 const server = app.listen(PORT, HOST, () => {
   console.log(`Server listening on http://${HOST}:${PORT}`);
